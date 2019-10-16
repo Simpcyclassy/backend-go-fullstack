78374961ffe4adf52f40d21d53c8fe355d83ffea
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-10-16 06:19:16 +0100

    add middleware

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Request received!');
+  next();
+});
 
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Your request was successful!' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Response sent successfully!');
+});
 
 module.exports = app;
