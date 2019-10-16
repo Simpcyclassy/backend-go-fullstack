29b5e6fb5b10e5e20eabe682d261ce3baac55b7c
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-10-16 05:38:19 +0100

    add express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('This is my response!');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
