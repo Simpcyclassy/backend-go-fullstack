171f2df7927ef39baf22e859163db4c5624bf204
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-10-17 11:29:09 +0100

    create signup controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,29 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
 exports.signup = (req, res, next) => {
-
-};
-
+    bcrypt.hash(req.body.password, 10).then(
+      (hash) => {
+        const user = new User({
+          email: req.body.email,
+          password: hash
+        });
+        user.save().then(
+          () => {
+            res.status(201).json({
+              message: 'User added successfully!'
+            });
+          }
+        ).catch(
+          (error) => {
+            res.status(500).json({
+              error: error
+            });
+          }
+        );
+      }
+    );
+  };
 exports.login = (req, res, next) => {
 
 };
