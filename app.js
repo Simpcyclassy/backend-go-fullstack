ba7b043192ea86bcbd69c3ab9d7c07faeb324610
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-10-17 04:21:20 +0100

    connect to mongodb

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+// MONGODB CONNECTION: mongodb+srv://Simpcy:<password>@cluster0-ejso1.mongodb.net/test?retryWrites=true&w=majority
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://Simpcy:MEwIX7lGPlNRkb4v@cluster0-ejso1.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+  });
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Thing created successfully!'
+    });
   });
 
 app.use('/api/stuff', (req, res, next) => {
